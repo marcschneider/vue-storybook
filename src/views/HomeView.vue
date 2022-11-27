7504aff2f7d1de03f75b30a588dde4bf3802d1e6
Marc Schneider <marc@unknown.digital>
2022-11-27 13:25:18 +0100

    Add more sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
+import Header from '../components/Header.vue'
 import Title from '../components/Title.vue'
+import Feature from '../components/Feature.vue'
+import Footer from '../components/Footer.vue'
 </script>
 
 <template>
   <main>
+    <Header />
     <Title />
+    <Feature />
+    <Footer />
   </main>
 </template>
